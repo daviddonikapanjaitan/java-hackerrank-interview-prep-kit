5062019594a70049115dbf44b0bb14be1de52b5a
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2025-06-07 16:32:43 +0700

    fixing code common child string manipulation

--- java/src/StringManipulation/P5CommonChild/CommonChildSolution.java
+++ java/src/StringManipulation/P5CommonChild/CommonChildSolution.java
@@ -4,26 +4,26 @@ package StringManipulation.P5CommonChild;
 // https://www.youtube.com/watch?v=ItRZRx8kvwY
 public class CommonChildSolution {
 
-    public static int commonChild(String s1, String s2){
-        int[][] array = new int[s1.length() + 1][s2.length() + 1];
-        for (int i = 0; i <= s1.length(); i++) {
-            for (int j = 0; j <= s2.length(); j++) {
-                if(i == 0 || j == 0){
-                    array[i][j] = 0;
-                } else if(s1.charAt(i - 1) == s2.charAt(j - 1)){
-                    array[i][j] = array[i-1][j-1] + 1;
-                }else{
-                    array[i][j] = Math.max(array[i][j-1], array[j-1][i]);
+    public static int commonChild(String str1, String str2){
+        int L[][] = new int[str1.length()+1][str2.length()+1];
+        for(int i=0;i<=str1.length();i++){
+            for(int j=0;j<=str2.length();j++){
+                if(i==0 || j==0)
+                    L[i][j]=0;
+                else if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    L[i][j] = L[i-1][j-1]+1;
+                }
+                else{
+                    L[i][j] = Math.max(L[i-1][j],L[i][j-1]);
                 }
             }
         }
-
-        return array[s1.length()][s2.length()];
+        return L[str1.length()][str2.length()];
     }
 
     public static void main(String[] args) {
-        String s1 = "ABCD";
-        String s2 = "ABDC";
+        String s1 = "SHINCHAN";
+        String s2 = "NOHARAAA";
         int result = commonChild(s1, s2);
 
         System.out.println(result);
